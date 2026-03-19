c117fdb107379ed0cff80a7d2d15358f312c634a
Sharon Aicler <saichler@gmail.com>
2026-03-19 05:14:49 -0700

    remove go.mod

--- go/run-local.sh
+++ go/run-local.sh
@@ -43,7 +43,8 @@ echo "rm -rf demo" >> ./kill_demo.sh
 echo "rm -rf /data/postgres/admin" >> ./kill_demo.sh
 echo "pkill -9 demo" >> ./kill_demo.sh
 chmod +x ./kill_demo.sh
-
+echo "Key=$ANTHROPIC_API_KEY"
+read -p "Press Enter"
 ./log-vnet_demo &
 ./vnet_demo &
 sleep 1
